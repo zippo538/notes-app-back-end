ed7f3a5b817982983ea2f810c5c133896de55c3b
Pijar Pahlawan <pijarpahlawanq@students.amikom.ac.id>
2022-08-27 21:25:47 +0700

    fixing bug

--- src/server.js
+++ src/server.js
@@ -1,20 +1,18 @@
-const Hapi = require("hapi")
-const route = require("./routes.js")
+const Hapi = require("@hapi/hapi");
+const route = require("./routes");
 
 const init = async () => {
-    const server = Hapi.server({
-        port : 8003,
-        host : process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-        routes: {
-            cors: {
-              origin: ['*'],
-            },
-          },
-    })
-    server.route(route)
-    await server.start()
-        console.log(`Server sedang berjalan pada ${server.info.uri}`)
-}
-init()
-
- 
+  const server = Hapi.server({
+    port: 8080,
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+  server.route(route);
+  await server.start();
+  console.log(`Server sedang berjalan pada ${server.info.uri}`);
+};
+init();
